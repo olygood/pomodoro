717323c0d1baa3c3ac47521e9713941eb63b9949
olijs <olivier_huttmacher@outlook.com>
2021-09-15 13:43:00 +0200

    Updates

---
+++ precache-manifest.fa651cdc2652abfb6a31bfa9f02129ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "924f2dc7d755286afcdf",
+    "url": "/pomodoro/static/css/main.531315f0.chunk.css"
+  },
+  {
+    "revision": "924f2dc7d755286afcdf",
+    "url": "/pomodoro/static/js/main.c6fb2b01.chunk.js"
+  },
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  },
+  {
+    "revision": "8795d4af043ddfe37042",
+    "url": "/pomodoro/static/js/2.7cb9f535.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pomodoro/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "508d1c4b8d8f34b0ab8b59a740b4a077",
+    "url": "/pomodoro/index.html"
+  }
+];
